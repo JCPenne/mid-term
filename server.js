d71f3c0d67cad476c1088d24aacc7c1e90b7bde0
JCPenne <jackcpalmer@icloud.com>
2022-08-18 15:27:49 -0700

    wand turned to conditional /admin button

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use(express.static("public"));
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
-const usersRoutes = require("./routes/users");
+// const usersRoutes = require("./routes/users");
 // const widgetsRoutes = require("./routes/widgets");
 const landing_page = require("./routes/landing_page");
 const admin_login = require("./routes/admin_login");
