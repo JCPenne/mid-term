1df63ea670984ac3bf8b78cf08275be82506155f
JCPenne <jackcpalmer@icloud.com>
2022-08-18 15:31:29 -0700

    wand works for both admin and user (takes both to their respective homepages)

--- routes/user_login.js
+++ routes/user_login.js
@@ -1,19 +1,20 @@
 const express = require("express");
 const { getFeaturedCards } = require("../database");
 const router = express.Router();
-var cookieParser = require('cookie-parser')
-
+var cookieParser = require("cookie-parser");
 
 // Note: the "/" really means /user, this is specified in the server.js file where all the routes are mounted.
 router.get("/", (req, res) => {
   res.cookie("account", 2);
+  const userID = 2;
 
   //////////////////////////////////////////////// This code is just for testing purposes and will be removed later.
   getFeaturedCards().then((featuredCards) => {
     const templateVars = {
       featuredCards,
+      userID,
     };
-    res.render("landing", templateVars);
+    res.render("user_homepage", templateVars);
   });
   ////////////////////////////////////////////////
 
