6ca50b2b69fef724323ec15eaf6cc021c556d29f
Noah Palin <noah.m.palin@gmail.com>
2022-08-18 17:59:36 +0000

    Add user and admin login routes.

--- server.js
+++ server.js
@@ -40,11 +40,15 @@ app.use(express.static("public"));
 const usersRoutes = require("./routes/users");
 // const widgetsRoutes = require("./routes/widgets");
 const landing_page = require("./routes/landing_page");
+const admin_login = require("./routes/admin_login");
+const user_login = require("./routes/user_login");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 // app.use("/api/widgets", widgetsRoutes(db));
 app.use("/home", landing_page);
+app.use("/admin", admin_login);
+app.use("/user", user_login)
 // Note: mount other resources here, using the same pattern above
 
 // Home page
