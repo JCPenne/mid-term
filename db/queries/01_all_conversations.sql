3ea541dec437ee41293ff597572cfed8c766e67d
JCPenne <jackcpalmer@icloud.com>
2022-08-22 12:51:16 -0700

    refine first query to include card name

--- db/queries/01_all_conversations.sql
+++ db/queries/01_all_conversations.sql
@@ -1,9 +1,9 @@
 SELECT conversations.id as id,
-messages.sender_id as sender,
-messages.receiver_id as receiver,
+cards.name as card_name,
 users.name as name
 FROM conversations
 JOIN messages ON conversations.id = messages.conversation_id
 JOIN users ON messages.sender_id = users.id
+JOIN cards ON conversations.card_id = cards.id
 WHERE messages.receiver_id = 1
-GROUP BY conversations.id, sender, receiver, name;
+GROUP BY conversations.id, users.name, cards.name;
