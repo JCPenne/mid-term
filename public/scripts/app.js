de975f29f098c51793a4c8d22d16226a6734f2f7
Noah Palin <noah.m.palin@gmail.com>
2022-08-23 20:02:56 +0000

    Create a like-button feature.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,21 +1,3 @@
-// Client facing scripts here
-const createCardElement = (cardData) => {
-  return `<div class="card-tile">
-  <img src="${cardData.image_url}"></img>
-  <div class="name-and-price">
-    <h3>${cardData.name}</h3>
-    <h3>$${cardData.price}</h3>
-  </div>
-</div>`;
-};
-
-const renderCards = (cards) => {
-  $(".card-tiles-container").empty();
-  for (const card of cards) {
-    $(".card-tiles-container").append(createCardElement(card));
-  }
-};
-
 $(document).ready(() => {
   $("#new-listing-form").submit((event) => {
     event.preventDefault();
@@ -38,4 +20,35 @@ $(document).ready(() => {
       renderCards(data.allCards);
     });
   });
+
+  $(".favorite-button").click((event) => {
+    event.preventDefault();
+    console.log($(".favorite-button"));
+    $.post("/like",{id: $(event.target).data("id")})
+    .fail(() => {
+      alert(`Could not like card.`);
+    })
+    .done((data) => {
+      $(event.target).addClass("highlight-red");
+      console.log("HERE " + data);
+    });
+  });
 });
+
+// Client facing scripts here
+const createCardElement = (cardData) => {
+  return `<div class="card-tile">
+  <img src="${cardData.image_url}"></img>
+  <div class="name-and-price">
+    <h3>${cardData.name}</h3>
+    <h3>$${cardData.price}</h3>
+  </div>
+</div>`;
+};
+
+const renderCards = (cards) => {
+  $(".card-tiles-container").empty();
+  for (const card of cards) {
+    $(".card-tiles-container").append(createCardElement(card));
+  }
+};
