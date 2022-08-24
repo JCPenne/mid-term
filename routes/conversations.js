411581f659435d01e59bf6226fe2c1fc5238891c
Jas Mann <jasforshort@hotmail.ca>
2022-08-24 20:06:54 +0000

    Progress through messaging feature

--- routes/conversations.js
+++ routes/conversations.js
@@ -1,11 +1,22 @@
 const express = require("express");
-const { getConversations } = require("../database");
+const { getConversations, getMessages } = require("../database");
 const router = express.Router();
 
 router.get("/", (req, res) => {
   getConversations().then((conversations) => {
     const templateVars = {
       conversations,
+      userID: req.cookies.account,
+    };
+    res.render("conversations", templateVars);
+  });
+});
+module.exports = router;
+
+router.get("/", (req, res) => {
+  getMessages().then((messages) => {
+    const templateVars = {
+      messages,
       userID: req.cookies.account,
     };
     res.render("conversations", templateVars);
